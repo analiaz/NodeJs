b36cd936bceca8cb0ff5237a85339b62ef2d6660
Analia Lujan Zunino <analia.zunino@accenture.com>
2023-04-03 16:26:59 -0300

    usar promesas para agarrar errores

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,17 +1,10 @@
+
+const { crearArchivo } = require('./helpers/multiplicar');
+
 console.clear();
 
-const fs = require('fs');
+const base = 8;
 
-console.log('==== tabla del cinco ====')
-
-const base = 5;
-let salida = '';
-
-for (let i = 1; i <=10; i++) {
-    salida += `${base} x ${i} = ${base * i}\n`;
-}
-
-fs.writeFile('tabla-5.txt', salida, (err) => {
-    if (err) throw err;
-    console.log('tabla-5.txt creada');
-})
+crearArchivo( base )
+    .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
+    .catch(err => console.log(err));
