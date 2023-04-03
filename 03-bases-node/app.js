174e1b99e3579b50e2d256749cde597f9643b7d9
Analia Lujan Zunino <analia.zunino@accenture.com>
2023-04-03 15:47:32 -0300

    creacion txt tabla 5 por consola FS

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,9 +1,17 @@
 console.clear();
 
+const fs = require('fs');
+
 console.log('==== tabla del cinco ====')
 
 const base = 5;
+let salida = '';
 
 for (let i = 1; i <=10; i++) {
-    console.log(`${base} x ${i} =${base * i}`);
+    salida += `${base} x ${i} = ${base * i}\n`;
 }
+
+fs.writeFile('tabla-5.txt', salida, (err) => {
+    if (err) throw err;
+    console.log('tabla-5.txt creada');
+})
