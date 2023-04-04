a208b9c1963dcf344e4f9ba6c949141e63db5d48
Analia Lujan Zunino <analia.zunino@accenture.com>
2023-04-04 15:30:49 -0300

    agregar descripcion a cada ocpion de la ruta

--- 03-bases-node/config/yargs.js
+++ 03-bases-node/config/yargs.js
@@ -2,11 +2,13 @@ const argv = require('yargs')
             .option('b', {
                 alias: 'base',
                 type: 'number',
+                describe: 'Es la base de la tabla de multiplicar'
             })
             .option('l', {
                 alias: 'listar',
                 type: 'boolean',
-                default: false
+                default: false,
+                describe: 'muestra la tabla de multiplicar en consola'
             }
             )
             .check( ( argv, options ) => {
