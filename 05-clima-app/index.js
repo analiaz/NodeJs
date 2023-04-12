0d07ee2d48d0a51f7a7ae49e23d0f472c13bb1c9
Analia Lujan Zunino <analia.zunino@accenture.com>
2023-04-12 15:13:32 -0300

    Buscar Ciudad Inicial

--- 05-clima-app/index.js
+++ 05-clima-app/index.js
@@ -1,13 +1,33 @@
 const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer");
+const Busquedas = require("./models/busquedas");
 
 
 const main = async() => {
     
+    const busquedas = new Busquedas();
     let opt;
 
     do{
         opt = await inquirerMenu();
-        console.log({opt});
+
+        switch(opt) {
+            case 1:
+
+                const lugar = await leerInput('Ciudad; ');
+                console.log(lugar);
+
+                console.log('\nInformacion de la ciudad\n');
+                console.log('Ciudad;', );
+                console.log('Lat;', );
+                console.log('Long;', );
+                console.log('Tempreatura;', );
+                console.log('Minima;', );
+                console.log('Maxima;', );
+
+                
+
+                break;
+        }
 
         if (opt !== 0) await pausa();
     } while ( opt !== 0)
---
+++ 05-clima-app/models/busquedas.js
@@ -0,0 +1,18 @@
+
+
+class Busquedas {
+    historial = ['Tegucigalpa', 'Madrid', 'San Jose'];
+
+    constructor() {
+
+    }
+
+    async ciudad(lugar = '') {
+
+        console.log(lugar);
+
+        return [];
+    }
+}
+
+module.exports = Busquedas;
